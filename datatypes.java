7d08f6cba64d5c09767c2c0c82544d50faa275cc
Geetansh Gupta <67698473+geetansh73@users.noreply.github.com>
2021-07-10 18:44:50 +0530

    Create datatypes.java

---
+++ datatypes.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+     Scanner sc=new Scanner(System.in);
+int     i=sc.nextInt();
+
+  double   d=sc.nextDouble();
+  double e=4.0;
+  sc.nextLine();
+        String s=sc.nextLine(); 
+        int a=4;
+     System.out.println(i+a);
+     System.out.println(d+e);
+    System.out.println("HackerRank "+s);
+    }
+}
